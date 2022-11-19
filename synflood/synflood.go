8571bbeb97ccc6bb0b7e8a52068ea0adf1110505
Francesco Di Donato <didonato.fr@gmail.com>
2022-11-19 15:56:42 +0100

    new: resolve host
    Signed-off-by: Francesco Di Donato <didonato.fr@gmail.com>

--- synflood/synflood.go
+++ synflood/synflood.go
@@ -2,10 +2,13 @@ package synflood
 
 import (
 	"context"
+	"math/rand"
+	"net"
 	"time"
 
 	"github.com/didof/swissknife/internal/logger"
 	"github.com/didof/swissknife/internal/version"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
 
@@ -28,14 +31,17 @@ func Run(ctx context.Context, target string, opts SynFloodOptions) {
 		zap.String("buildData", ver.BuildDate),
 	)
 
-	var cancel context.CancelFunc
+	var timeoutCancel context.CancelFunc
 	if opts.FloodDurationMilliseconds != -1 {
-		ctx, cancel = context.WithTimeout(ctx, time.Millisecond*time.Duration(opts.FloodDurationMilliseconds))
+		ctx, timeoutCancel = context.WithTimeout(ctx, time.Millisecond*time.Duration(opts.FloodDurationMilliseconds))
 	}
 
 	go func() {
-		if err := do(ctx, opts); err != nil {
-			cancel()
+		if err := do(ctx, target, opts); err != nil {
+			if timeoutCancel != nil {
+				timeoutCancel()
+			}
+
 			log.Fatal("an error occured on flooding process", zap.String("error", err.Error()))
 		}
 	}()
@@ -43,7 +49,9 @@ func Run(ctx context.Context, target string, opts SynFloodOptions) {
 	for {
 		select {
 		case <-ctx.Done():
-			cancel()
+			if timeoutCancel != nil {
+				timeoutCancel()
+			}
 			return
 		default:
 			continue
@@ -51,6 +59,32 @@ func Run(ctx context.Context, target string, opts SynFloodOptions) {
 	}
 }
 
-func do(ctx context.Context, opts SynFloodOptions) error {
+var ErrDNSLookup = errors.New("dns lookup")
+
+func do(ctx context.Context, host string, opts SynFloodOptions) error {
+	rand.Seed(time.Now().Unix())
+
+	_, err := resolveHost(ctx, host)
+	if errors.Is(err, ErrDNSLookup) {
+		return errors.Wrap(err, "unable to resolve host")
+	}
+
 	return nil
+}
+
+func resolveHost(ctx context.Context, input string) (net.IP, error) {
+	if ip := net.ParseIP(input); ip != nil {
+		log.Debug("already an IP address, skipping DNS resolution", zap.String("host", input))
+		return ip, nil
+	}
+
+	ipRecords, err := net.DefaultResolver.LookupIP(ctx, "ip4", input)
+	if err != nil {
+		return nil, ErrDNSLookup
+	}
+
+	ip := ipRecords[0]
+	log.Debug("DNS lookup succeeded", zap.String("DNS", input), zap.String("IP", ip.String()))
+
+	return ip, nil
 }
